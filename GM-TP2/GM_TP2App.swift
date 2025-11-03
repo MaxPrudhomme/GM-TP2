57d0e0c34e8a25382985a658b292555de5a55318
Max Prudhomme <account@maxprudhomme.com>
2025-11-03 09:42:50 +0100

    Initial Commit

---
+++ GM-TP2/GM_TP2App.swift
@@ -0,0 +1,17 @@
+//
+//  GM_TP2App.swift
+//  GM-TP2
+//
+//  Created by Max PRUDHOMME on 03/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct GM_TP2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
